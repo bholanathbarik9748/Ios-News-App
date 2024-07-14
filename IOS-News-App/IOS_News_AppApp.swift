49a08c3770ef5e9e526611f444b1f65b6eeda4ca
Bholanath <bholanathbarik9748@gmail.com>
2024-07-14 18:20:22 +0530

    Create Project

---
+++ IOS-News-App/IOS_News_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_News_AppApp.swift
+//  IOS-News-App
+//
+//  Created by Bholanath Barik on 14/07/24.
+//
+
+import SwiftUI
+
+@main
+struct IOS_News_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
