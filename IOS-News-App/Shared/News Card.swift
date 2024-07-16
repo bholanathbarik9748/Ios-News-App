bc9a0886d4843aa074bc7d436d332829bb24e961
Bholanath <bholanathbarik9748@gmail.com>
2024-07-17 00:21:38 +0530

    Feat : Create UI for News Card and function for time convertion

--- IOS-News-App/Shared/News Card.swift
+++ IOS-News-App/Shared/News Card.swift
@@ -8,8 +8,46 @@
 import SwiftUI
 
 struct News_Card: View {
+    private let helper = Helper()
+    
+    let imgUrl = "https://media.wired.com/photos/6672c421e0704c563b4e7b77/191:100/w_1280,c_limit/GettyImages-80973767.jpg"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            AsyncImage(url: URL(string: imgUrl)) { image in
+                image
+                    .resizable()
+                    .scaledToFill()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(height: 150)
+                    .clipped()
+            } placeholder: {
+                ProgressView()
+            }
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text("This Ancient Technology Is Helping Millions Stay Cool")
+                    .font(.headline)
+                    .foregroundColor(.primary)
+                
+                Text("Cheap, low-energy evaporative cooling devices are keeping water, food......")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                    .lineLimit(3)
+                
+                if let formatedTime = helper.convertTimestampToReadableDateAndTime("2024-07-09T15:42:42Z") {
+                    Text(formatedTime)
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
+            }
+            .padding()
+        }
+        .background(Color(.systemBackground))
+        .cornerRadius(10)
+        .shadow(radius: 5)
+        .padding(.horizontal)
+        
     }
 }
 
--- IOS-News-App/utils/helper.swift
+++ IOS-News-App/utils/helper.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+class Helper {
+    func convertTimestampToReadableDateAndTime(_ timestamp: String) -> String? {
+        // Define the input date format
+        let inputFormatter = ISO8601DateFormatter()
+        inputFormatter.formatOptions = [.withInternetDateTime] // Remove fractional seconds if not needed
+        
+        // Parse the timestamp to a Date object
+        guard let date = inputFormatter.date(from: timestamp) else {
+            return nil
+        }
+        
+        // Define the output date format
+        let outputFormatter = DateFormatter()
+        outputFormatter.dateStyle = .medium
+        outputFormatter.timeStyle = .medium
+        outputFormatter.amSymbol = "AM"
+        outputFormatter.pmSymbol = "PM"
+        
+        // Convert the Date object to a readable string
+        let readableDateAndTime = outputFormatter.string(from: date)
+        return readableDateAndTime
+    }
+}
