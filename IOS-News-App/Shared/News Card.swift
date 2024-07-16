54d568e483af3eb6f94695a778e96bd1fc1dbdf5
Bholanath <bholanathbarik9748@gmail.com>
2024-07-17 00:21:29 +0530

    Feat : Create UI for News Card and function for time convertion

---
+++ IOS-News-App/Shared/News Card.swift
@@ -0,0 +1,18 @@
+//
+//  News Card.swift
+//  IOS-News-App
+//
+//  Created by Bholanath Barik on 16/07/24.
+//
+
+import SwiftUI
+
+struct News_Card: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    News_Card()
+}
---
+++ IOS-News-App/utils/helper.swift
@@ -0,0 +1,8 @@
+//
+//  helper.swift
+//  IOS-News-App
+//
+//  Created by Bholanath Barik on 16/07/24.
+//
+
+import Foundation
